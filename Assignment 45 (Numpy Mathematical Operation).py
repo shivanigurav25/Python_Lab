6ff17fdbb07103b62e119f5766463ee1ba0bd98c
shivanigurav25 <152187397+shivanigurav25@users.noreply.github.com>
2024-07-26 10:30:07 +0530

    Add files via upload

---
+++ Assignment 45 (Numpy Mathematical Operation).py
@@ -0,0 +1,20 @@
+#Compute the standard deviation of the NumPy array
+#Input: arr = [20, 2, 7, 1, 34]
+
+
+
+import numpy as np
+
+# Input array
+arr = np.array([20, 2, 7, 1, 34])
+
+# Compute the standard deviation
+std_deviation = np.std(arr)
+
+# Print the standard deviation
+print("Standard Deviation:", std_deviation)
+
+
+
+#Output
+#Standard Deviation: 12.576167937809991
